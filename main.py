214f4004c27e89c031dc60654b862e79e6f111ae
Mykola Masiuk <masiuk.mykola@globaldev.tech>
2025-06-22 10:49:05 +0300

    Add initial implementation of bot assistant and logging utilities

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from task_1.caching_fibonacci import caching_fibonacci
 from task_2.generator_numbers import generator_numbers, sum_profit
 from task_4.bot_assistant import main as start_bot
 
-# print(caching_fibonacci(10))
+print(caching_fibonacci(10))
 
 
-# text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-# total_income = sum_profit(text, generator_numbers)
-# print(f"Загальний дохід: {total_income}")
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
 
 start_bot()
